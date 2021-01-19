974469e76681bfb3048918f5df128a2dfe600751
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 12:57:48 -0300

    Media 2

--- Media2/Media2/Program.cs
+++ Media2/Media2/Program.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace Media2 {
     class Program {
         static void Main(string[] args) {
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (a + b + c) / 3;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
         }
     }
 }
