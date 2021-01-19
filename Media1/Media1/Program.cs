a79980323ad38023b0c3e889df3272d4ed6bac52
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 12:33:52 -0300

    Média 1

---
+++ Media1/Media1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Media1 {
+    class Program {
+        static void Main(string[] args) {
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (a + b) / 2; 
+
+            Console.WriteLine("MEDIA=" + media.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
