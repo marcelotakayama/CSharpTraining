72c4822098cdaa5e88c047c6778eb76656d1dd6e
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 18:16:08 -0300

    Gasto com gasolina

--- GastoCombustivel/GastoCombustivel/Program.cs
+++ GastoCombustivel/GastoCombustivel/Program.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace GastoCombustivel {
     class Program {
         static void Main(string[] args) {
+            int tempo = int.Parse(Console.ReadLine());
+            int velocidade = int.Parse(Console.ReadLine());
+
+            double result = (tempo * velocidade) / 12;
+
+            Console.WriteLine(result.ToString("F3", CultureInfo.InvariantCulture));
         }
     }
 }
