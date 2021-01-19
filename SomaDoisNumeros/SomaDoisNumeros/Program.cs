2854709435db5de46e07577083906dcbb504e4c7
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 11:40:13 -0300

    First exercise

---
+++ SomaDoisNumeros/SomaDoisNumeros/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaDoisNumeros {
+    class Program {
+        static void Main(string[] args) {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int X = A + B;
+            Console.WriteLine("X = " + X);
+        }
+    }
+}
