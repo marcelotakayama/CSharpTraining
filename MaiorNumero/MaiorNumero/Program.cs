d97da58218316b0087c76a90fd0348cb894ee50f
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 16:50:54 -0300

    maiorNumero

---
+++ MaiorNumero/MaiorNumero/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace MaiorNumero {
+    class Program {
+        static void Main(string[] args) {
+            string[] entrada = Console.ReadLine().Split(' ');
+            int a = int.Parse(entrada[0], CultureInfo.InvariantCulture);
+            int b = int.Parse(entrada[1], CultureInfo.InvariantCulture);
+            int c = int.Parse(entrada[2], CultureInfo.InvariantCulture);
+
+            if(a > b && a > c) {
+                Console.WriteLine(a + " eh o maior");
+            } else if (b > a && b > c) {
+                Console.WriteLine(b + " eh o maior");
+            } else {
+                Console.WriteLine(c + " eh o maior");
+            }
+        }
+    }
+}
