6054a59cfcf23ef7c524b8fd275e978cefeae528
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 17:25:28 -0300

    consumo

--- Consumo/Consumo/Program.cs
+++ Consumo/Consumo/Program.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Consumo {
     class Program {
         static void Main(string[] args) {
+            int X = int.Parse(Console.ReadLine());
+            double Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = X / Y;
+
+            Console.WriteLine(result.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
         }
     }
 }
