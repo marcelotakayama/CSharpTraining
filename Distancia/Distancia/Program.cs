199d00195c3662046c939691df6d61baa1b7e22f
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 18:00:41 -0300

    exercicio da distancia

--- Distancia/Distancia/Program.cs
+++ Distancia/Distancia/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace Distancia {
     class Program {
         static void Main(string[] args) {
+            int distancia = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((distancia * 2) + " minutos");
         }
     }
 }
