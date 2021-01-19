579e0237e4d89deb9943c4844a49432a4c8fe2e9
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 15:01:55 -0300

    Salario

---
+++ Salario1/Salario1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Salario1 {
+    class Program {
+        static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = b * c;
+
+            Console.WriteLine("NUMBER = " + a);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
