364e941387a427d9f189e228443859ad42abfc0b
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 15:22:14 -0300

    Salário com bônus

---
+++ Salaraio2/Salaraio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Salaraio2 {
+    class Program {
+        static void Main(string[] args) {
+            string nome = Console.ReadLine();
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double comissao = ((vendas / 100) * 15);
+            double resultado = salario + comissao;
+
+            Console.WriteLine("TOTAL = R$ " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
