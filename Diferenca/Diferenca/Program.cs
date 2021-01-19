dfb932a040dcbbeb92f1d94832cee26fdb986320
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 14:52:27 -0300

    diferenca

--- Diferenca/Diferenca/Program.cs
+++ Diferenca/Diferenca/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Diferenca {
     class Program {
         static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            int diferenca = (a * b - c * d);
+
+            Console.WriteLine("DIFERENCA = " + diferenca);
         }
     }
 }
