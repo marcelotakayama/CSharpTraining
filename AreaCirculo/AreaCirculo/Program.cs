cc6532be23e0aa60ce80448d055623cf854ec378
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 12:05:12 -0300

    Circle area

---
+++ AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AreaCirculo {
+    class Program {
+        static void Main(string[] args) {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = 3.14159 * (raio * raio);
+
+            Console.WriteLine("A={0:F4}" , area);
+        }
+    }
+}
