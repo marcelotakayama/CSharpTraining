b63c76dc206f7f88ecd68b96790a4ca665bb3c2a
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 17:41:49 -0300

    Diferenca entre dois pontos

---
+++ DistanciaEntreDoisPontos/DistanciaEntreDoisPontos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace DistanciaEntreDoisPontos {
+    class Program {
+        static void Main(string[] args) {
+            string[] entrada;
+            entrada = Console.ReadLine().Split(' ');
+
+            double x1 = double.Parse(entrada[0], CultureInfo.InvariantCulture);
+            double y1 = double.Parse(entrada[1], CultureInfo.InvariantCulture);
+
+            entrada = Console.ReadLine().Split(' ');
+
+            double x2 = double.Parse(entrada[0], CultureInfo.InvariantCulture);
+            double y2 = double.Parse(entrada[1], CultureInfo.InvariantCulture);
+
+            double result = Math.Sqrt((x2-x1) * (x2-x1) + (y2 - y1) * (y2 - y1));
+
+            Console.WriteLine(result.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
