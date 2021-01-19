571bf9ee98f40b08119027c069e7f707ade53a5c
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 16:13:01 -0300

    Programa de cálu=culo da área da esfera

---
+++ Esfera/Esfera/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Esfera {
+    class Program {
+        static void Main(string[] args) {
+            double pi = 3.14159;
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = (4 / 3.0) * pi * (raio * raio * raio);
+
+            Console.WriteLine("VOLUME = " + resultado.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
