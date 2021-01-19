bda71d915afff6689dd8e5d29d02ce159d313cd9
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-19 16:39:30 -0300

    Programa para calcular as áreas das formas

---
+++ Areas/Areas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Areas {
+    class Program {
+        static void Main(string[] args) {
+            string[] entrada;
+
+            entrada = Console.ReadLine().Split(' ');
+
+            double a = double.Parse(entrada[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(entrada[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(entrada[2], CultureInfo.InvariantCulture);
+
+            double pi = 3.14159;
+
+            double triangulo = (a * c)/2;
+            double circulo = pi * (c * c);
+            double trapezio = ((a + b) * c) / 2;
+            double quadrado = b * b;
+            double retangulo = a * b;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
