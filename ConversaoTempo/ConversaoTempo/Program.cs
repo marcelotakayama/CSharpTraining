206a0f57d2bd9851b70eec07494fb0f52787b85c
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-20 10:27:02 -0300

    programa de conversão de horas

---
+++ ConversaoTempo/ConversaoTempo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversaoTempo {
+    class Program {
+        static void Main(string[] args) {
+            int entrada = int.Parse(Console.ReadLine());
+
+            int h = entrada / 3600;
+            entrada -= h * 3600;
+
+            int m = entrada / 60;
+            entrada -= m * 60;
+
+            int s = entrada;
+
+            Console.WriteLine(h + ":" + m + ":" + s);
+        }
+    }
+}
