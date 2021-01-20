bb77c63a5f39538fe83e6a0e94c297b8d46b3d0b
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-20 12:00:48 -0300

    Programa de notas e moedas

--- NotasMoedas/NotasMoedas/Program.cs
+++ NotasMoedas/NotasMoedas/Program.cs
@@ -7,11 +7,25 @@ using System.Threading.Tasks;
 namespace NotasMoedas {
     class Program {
         static void Main(string[] args) {
-            double entrada = double.Parse(Console.ReadLine());
-            int notas = (int)entrada;
-            int moedas = (int)((entrada - notas) * 100);
-
-
+            double dinheiro = double.Parse(Console.ReadLine());
+            double[] valor = { 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
+            int quantidadeNotas;
+            
+            Console.WriteLine("NOTAS:");
+            for(int i=0; i<6; i++) {
+                quantidadeNotas = (int)(dinheiro / valor[i]);
+                Console.WriteLine(quantidadeNotas + " nota(s) de R$ " + valor[i].ToString("0.00"));
+                dinheiro -= quantidadeNotas * valor[i];
+                dinheiro = Math.Round(dinheiro, 2);
+            }
+            
+            Console.WriteLine("MOEDAS:");
+            for(int i=6; i<valor.Length; i++) {
+                quantidadeNotas = (int)(dinheiro / valor[i]);
+                Console.WriteLine(quantidadeNotas + " moeda(s) de R$ " + valor[i].ToString("0.00"));
+                dinheiro -= quantidadeNotas * valor[i];
+                dinheiro = Math.Round(dinheiro, 2);
+            }
         }
     }
 }
