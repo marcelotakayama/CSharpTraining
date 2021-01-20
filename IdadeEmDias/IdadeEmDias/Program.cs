f7d55848be8b4b1a3269223ea7390f32d4db6c10
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-20 10:46:53 -0300

    Programa da idade em dias

---
+++ IdadeEmDias/IdadeEmDias/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdadeEmDias {
+    class Program {
+        static void Main(string[] args) {
+            int entrada = int.Parse(Console.ReadLine());
+
+            int ano = entrada / 365;
+            entrada -= ano * 365;
+
+            int mes = entrada / 30;
+            entrada -= mes * 30;
+
+            int dias = entrada;
+
+            Console.WriteLine(ano + " ano(s)");
+            Console.WriteLine(mes + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+        }
+    }
+}
