5957e5caa014ed8ee7f5f8cf3c20cb28cbc32f1e
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-20 12:13:59 -0300

    Valores aceitos e não aceitos

--- TesteSelecao/TesteSelecao/Program.cs
+++ TesteSelecao/TesteSelecao/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace TesteSelecao {
     class Program {
         static void Main(string[] args) {
+            string[] entrada = Console.ReadLine().Split(' ');
+            int a = int.Parse(entrada[0]);
+            int b = int.Parse(entrada[1]);
+            int c = int.Parse(entrada[2]);
+            int d = int.Parse(entrada[3]);
+
+            if(b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0) {
+                Console.WriteLine("Valores aceitos");
+            } else {
+                Console.WriteLine("Valores não aceitos");
+            }
         }
     }
 }
