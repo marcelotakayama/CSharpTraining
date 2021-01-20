5cfbb934231e46458c2890239697a9860de7e14e
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-20 11:19:13 -0300

    Falta arrumar umas coisas

---
+++ NotasMoedas/NotasMoedas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotasMoedas {
+    class Program {
+        static void Main(string[] args) {
+            double entrada = double.Parse(Console.ReadLine());
+            int notas = (int)entrada;
+            int moedas = (int)((entrada - notas) * 100);
+
+
+        }
+    }
+}
