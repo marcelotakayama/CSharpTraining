bb1abde4e3d5b6b00afce9b8b1e7784d7826df51
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-21 10:42:37 -0300

    subindo projeto do triangulo

--- Triangulo/Triangulo/Program.cs
+++ Triangulo/Triangulo/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace Triangulo {
     class Program {
         static void Main(string[] args) {
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            double a = double.Parse(entrada[0]);
+            double b = double.Parse(entrada[1]);
+            double c = double.Parse(entrada[2]);
+
+            if(a + b > c || b + c > a || a + c > b) {
+                Console.WriteLine("Perimetro = " + (a + b + c));
+            } else {
+                Console.WriteLine("Area = " + (a + b) / 2);
+            }
         }
     }
 }
