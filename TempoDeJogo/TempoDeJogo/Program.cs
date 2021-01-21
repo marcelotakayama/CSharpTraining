d22af323f4d9499c830b2d92dfd3637ab343db74
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-21 13:20:48 -0300

    tempo de jogo

--- TempoDeJogo/TempoDeJogo/Program.cs
+++ TempoDeJogo/TempoDeJogo/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 namespace TempoDeJogo {
     class Program {
         static void Main(string[] args) {
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(entrada[0]);
+            int b = int.Parse(entrada[1]);
+
+            if (a > b) {
+                Console.WriteLine("O JOGO DUROU " +  (24 - a + b) + " HORA(S)");
+            } else if (a < b) {
+                Console.WriteLine("O JOGO DUROU " + (b - a) + " HORA(S)");
+            } else {
+                Console.WriteLine("O JOGO DUROU 24 HORA(S)");
+            }
         }
     }
 }
