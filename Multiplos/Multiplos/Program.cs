8ae6b15edd416f6ba83e0191206860102a4a2c1b
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-21 13:05:46 -0300

    multiplos

--- Multiplos/Multiplos/Program.cs
+++ Multiplos/Multiplos/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Multiplos {
     class Program {
         static void Main(string[] args) {
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(entrada[0]);
+            int b = int.Parse(entrada[1]);
+
+            if(a % b == 0 || b % a == 0) {
+                Console.WriteLine("Sao Multiplos");
+            } else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
         }
     }
 }
