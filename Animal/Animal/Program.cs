fe3ffdbb7d994b74d5552e5e6f2e62bc6dba66f4
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-21 16:31:59 -0300

    ex dos animais

--- Animal/Animal/Program.cs
+++ Animal/Animal/Program.cs
@@ -7,6 +7,42 @@ using System.Threading.Tasks;
 namespace Animal {
     class Program {
         static void Main(string[] args) {
+            string in1 = Console.ReadLine();
+            if (in1 == "vertebrado") {
+                string in2 = Console.ReadLine();
+                if (in2 == "ave") {
+                    string in3 = Console.ReadLine();
+                    if(in3 == "carnivoro") {
+                        Console.WriteLine("aguia");
+                    } else {
+                        Console.WriteLine("pomba");
+                    }
+                } else {
+                    string in3 = Console.ReadLine();
+                    if(in3 == "onivoro") {
+                        Console.WriteLine("homem");
+                    } else {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            } else {
+                string in2 = Console.ReadLine();
+                if(in2 == "inseto") {
+                    string in3 = Console.ReadLine();
+                    if(in3 == "hematofago") {
+                        Console.WriteLine("pulga");
+                    } else {
+                        Console.WriteLine("lagarta");
+                    }
+                } else {
+                    string in3 = Console.ReadLine();
+                    if (in3 == "hematofago") {
+                        Console.WriteLine("sanguessuga");
+                    } else {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
         }
     }
 }
