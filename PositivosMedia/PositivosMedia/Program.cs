dbc62c6debe971fc7f0f9ca613b5c7c04fad5233
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-22 11:06:46 -0300

    positivos e media

--- PositivosMedia/PositivosMedia/Program.cs
+++ PositivosMedia/PositivosMedia/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace PositivosMedia {
     class Program {
         static void Main(string[] args) {
+            int positivo = 0;
+            double media = 0;
+
+            for(int i = 0; i < 6; i++) {
+                double num = double.Parse(Console.ReadLine());
+                if (num > 0) {
+                    positivo++;
+                    media += num;
+                }
+            }
+            media = media / positivo;
+            Console.WriteLine(positivo + " valoras positivos");
+            Console.WriteLine(media);
         }
     }
 }
