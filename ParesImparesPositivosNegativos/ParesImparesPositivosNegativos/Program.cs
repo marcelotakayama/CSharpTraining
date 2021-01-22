72fa9f3c363b437a147579aeb83a3652e05cc91d
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-22 12:24:54 -0300

    ta faltando umas coisas

--- ParesImparesPositivosNegativos/ParesImparesPositivosNegativos/Program.cs
+++ ParesImparesPositivosNegativos/ParesImparesPositivosNegativos/Program.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 namespace ParesImparesPositivosNegativos {
     class Program {
         static void Main(string[] args) {
+            int par = 0;
+            int impar = 0;
+            int positivo = 0;
+            int negativo = 0;
+            for(int i=0; i<5; i++) {
+                int entrada = int.Parse(Console.ReadLine());
+                if(entrada % 2 == 0) {
+                    par++;
+                } else if (entrada % 2 != 0) {
+                    impar++;
+                } else if (entrada > 0) {
+                    positivo++;
+                } else if(entrada < 0) {
+                    negativo++;
+                }
+            }
+            Console.WriteLine(par + " valor(es) par(es)");
+            Console.WriteLine(impar + " valor(es) impar(es)");
+            Console.WriteLine(positivo + " valor(es) positivo(s)");
+            Console.WriteLine(negativo + " valor(es) negativo(s)");
         }
     }
 }
