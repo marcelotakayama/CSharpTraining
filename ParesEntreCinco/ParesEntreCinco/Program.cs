6eb11da29f76d90c7da0b07a0293c3388232cdc6
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-22 11:41:56 -0300

    numeros pares entre cinco

--- ParesEntreCinco/ParesEntreCinco/Program.cs
+++ ParesEntreCinco/ParesEntreCinco/Program.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace ParesEntreCinco {
     class Program {
         static void Main(string[] args) {
+            int pares = 0;
+
+            for (int i = 0; i < 5; i++) {
+                int entrada = int.Parse(Console.ReadLine());
+                if (entrada > 0) {
+                    pares++;
+                }
+            }
+            Console.WriteLine(pares + " valores pares");
         }
     }
 }
