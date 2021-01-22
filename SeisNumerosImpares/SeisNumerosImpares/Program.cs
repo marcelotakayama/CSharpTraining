09b04abdf758d87e5d6e05514d063eede54e9cec
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-22 15:13:25 -0300

    ex de seis numeros pares

--- SeisNumerosImpares/SeisNumerosImpares/Program.cs
+++ SeisNumerosImpares/SeisNumerosImpares/Program.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace SeisNumerosImpares {
     class Program {
         static void Main(string[] args) {
+            int entrada = int.Parse(Console.ReadLine());
+            int impar = 0;
+
+            while (impar < 6) {
+                if (entrada % 2 != 0) {
+                    Console.WriteLine(entrada);
+                    impar++;
+                } entrada++;
+            }
         }
     }
 }
