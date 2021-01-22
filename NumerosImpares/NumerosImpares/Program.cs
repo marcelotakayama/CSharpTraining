17ee4e71c9c75ad447cda18778b9b9771982ecc2
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-22 12:47:47 -0300

    numeros impares

--- NumerosImpares/NumerosImpares/Program.cs
+++ NumerosImpares/NumerosImpares/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace NumerosImpares {
     class Program {
         static void Main(string[] args) {
+            int entrada = int.Parse(Console.ReadLine());
+            int cont = 0;
+            for(int i=0; i<entrada; i++) {
+                if(i % 2 != 0) {
+                    Console.WriteLine(i);
+                    cont++;
+                }
+            }
         }
     }
 }
