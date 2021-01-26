616b1f2c60ade7dbd8285331ef5dec38b5d55c8b
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-26 17:44:50 -0300

    medias ponderadas

--- MediasPonderadas/MediasPonderadas/Program.cs
+++ MediasPonderadas/MediasPonderadas/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace MediasPonderadas {
     class Program {
         static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0; i<n; i++) {
+                string[] valores = Console.ReadLine().Split(' ');
+                double a = double.Parse(valores[0]);
+                double b = double.Parse(valores[1]);
+                double c = double.Parse(valores[2]);
+
+                double media = (a * 2 + b * 3 + c * 5) / 10;
+
+                Console.WriteLine(media);
+            }
         }
     }
 }
