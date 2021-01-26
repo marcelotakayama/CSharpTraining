86f164fe3cdb3b3e0e2c2b357fb3a8b5347426be
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-26 16:32:24 -0300

    medias ponderadas

--- MediasPonderadas/MediasPonderadas/Program.cs
+++ MediasPonderadas/MediasPonderadas/Program.cs
@@ -4,14 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tabuada {
+namespace MediasPonderadas {
     class Program {
         static void Main(string[] args) {
-            int inp = int.Parse(Console.ReadLine());
-
-            for(int i=1; i<=10; i++) {
-                Console.WriteLine(i + " X " + i + " = " + (i * inp));
-            }
         }
     }
 }
