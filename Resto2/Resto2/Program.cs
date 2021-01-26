baa0e18bf0da4160665f1361a70c7a815cd20326
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-26 15:51:16 -0300

    tabuada

--- Resto2/Resto2/Program.cs
+++ Resto2/Resto2/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Resto2 {
     class Program {
         static void Main(string[] args) {
+            int entrada = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=10000; i++) {
+                if(i % entrada == 2) {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
         }
     }
 }
