4ecc0b4fb7934e5db1085ea79b87b0eaed6b5380
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-26 12:09:17 -0300

    funciona mais ou menos

--- ParOuImpar/ParOuImpar/Program.cs
+++ ParOuImpar/ParOuImpar/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace ParOuImpar {
     class Program {
         static void Main(string[] args) {
+            int qtd = int.Parse(Console.ReadLine());
+            int saida = 0;
+            for (int i = 1; i <= qtd; i++) {
+                int entrada = int.Parse(Console.ReadLine());
+                
+                if (entrada == 0) {
+                    Console.WriteLine("NULL");
+                } else if (entrada > 0 && entrada % 2 == 0) {
+                    Console.WriteLine("EVEN POSITIVE");
+                } else if (entrada < 0 && entrada % 2 == 0) {
+                    Console.WriteLine("EVEN NEGATIVE");
+                } else if (entrada > 0 && entrada % 2 != 0) {
+                    Console.WriteLine("ODD POSITIVE");
+                } else if (entrada < 0 && entrada % 2 != 0) {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+            }
         }
     }
 }
