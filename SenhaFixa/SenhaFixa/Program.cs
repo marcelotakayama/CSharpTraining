bc685d54d490082ea5263e741d0a23f5550c06ec
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-27 15:46:19 -0300

    senha fixa

--- SenhaFixa/SenhaFixa/Program.cs
+++ SenhaFixa/SenhaFixa/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 namespace SenhaFixa {
     class Program {
         static void Main(string[] args) {
+            
+
+            while (true) {
+                int senha = int.Parse(Console.ReadLine());
+
+                if (senha != 2002) {
+                    Console.WriteLine("Senha invalida");
+                } else {
+                    Console.WriteLine("Acesso permitido");
+                    break;
+                }
+            }
         }
     }
 }
