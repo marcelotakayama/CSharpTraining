7c9be01291031e1546b88b62877cfc3b83d3b942
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-27 17:32:17 -0300

    tipo de combustivel

--- TipoDeCombustivel/TipoDeCombustivel/Program.cs
+++ TipoDeCombustivel/TipoDeCombustivel/Program.cs
@@ -7,6 +7,30 @@ using System.Threading.Tasks;
 namespace TipoDeCombustivel {
     class Program {
         static void Main(string[] args) {
+
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            for (int i=0; i<5; i++) {
+                int entrada = int.Parse(Console.ReadLine());
+
+                switch (entrada) {
+                    case 1:
+                        alcool++;
+                        break;
+                    case 2:
+                        gasolina++;
+                        break;
+                    case 3:
+                        diesel++;
+                        break;
+                }
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
         }
     }
 }
