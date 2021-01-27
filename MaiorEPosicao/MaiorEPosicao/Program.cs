a724310d81728628f5c8bef8bd207af0ca4fff78
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-27 10:24:42 -0300

    ex pronto de maior e posicao

--- MaiorEPosicao/MaiorEPosicao/Program.cs
+++ MaiorEPosicao/MaiorEPosicao/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace MaiorEPosicao {
     class Program {
         static void Main(string[] args) {
+            int[] vetor;
+            vetor = new int[10];
+            int maior = 0;
+            int posicao = 0;
+
+            for (int i=0; i<10; i++) {
+                int entrada = int.Parse(Console.ReadLine());
+                if(maior > entrada) {
+                    maior = maior;
+                } else {
+                    maior = entrada;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+
         }
     }
 }
