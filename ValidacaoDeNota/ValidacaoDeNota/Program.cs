9826fa838f061bcb138a9b8cf44357677f8022dc
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-27 15:52:47 -0300

    validacao de notas

---
+++ ValidacaoDeNota/ValidacaoDeNota/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacaoDeNota {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                int nota = int.Parse(Console.ReadLine());
+                if(nota < 6) {
+                    Console.WriteLine("nota invalida");
+                } else {
+                    Console.WriteLine("nota valida");
+                }
+            }
+        }
+    }
+}
