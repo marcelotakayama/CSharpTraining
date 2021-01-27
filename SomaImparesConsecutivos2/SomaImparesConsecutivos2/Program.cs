18429deff9a5e2504aa0e66bf78a366e6487ce99
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-27 15:07:17 -0300

    soma impares2

--- SomaImparesConsecutivos2/SomaImparesConsecutivos2/Program.cs
+++ SomaImparesConsecutivos2/SomaImparesConsecutivos2/Program.cs
@@ -7,6 +7,32 @@ using System.Threading.Tasks;
 namespace SomaImparesConsecutivos2 {
     class Program {
         static void Main(string[] args) {
+            int qtd = int.Parse(Console.ReadLine());
+            int maior = 0;
+            int menor = 0;
+            int impares = 0;
+
+            for(int i=0; i<qtd; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                int a = int.Parse(vet[0]);
+                int b = int.Parse(vet[1]);
+
+                if(a > b) {
+                    maior = a;
+                    menor = b;
+                } else {
+                    menor = a;
+                    maior = b;
+                }
+
+                for(int j=menor; j<maior; j++) {
+                    if(j % 2 != 0) {
+                        impares += j;
+                    }
+                }
+                Console.WriteLine(impares);
+                impares = 0;
+            }
         }
     }
 }
