6e84ea90aa13d85882f6531e537daa8e65c16944
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 16:49:48 -0300

    hohoh

--- HoHoHo/HoHoHo/Program.cs
+++ HoHoHo/HoHoHo/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace HoHoHo {
     class Program {
         static void Main(string[] args) {
+            int qtd = int.Parse(Console.ReadLine());
+            for(int i=0; i<qtd; i++) {
+                Console.WriteLine("Ho ");
+            }
+            Console.WriteLine("Ho!\n");
         }
     }
 }
