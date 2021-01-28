bf11481e76fa433d42245077feca61c5e313c75d
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 14:39:45 -0300

    sequencias crescentes

--- SequenciasCrescentes/SequenciasCrescentes/Program.cs
+++ SequenciasCrescentes/SequenciasCrescentes/Program.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace SequenciasCrescentes {
     class Program {
         static void Main(string[] args) {
+            int entrada = int.Parse(Console.ReadLine());
+            for(int i=1; i<=entrada; i++) {
+                Console.WriteLine(i);
+            }
         }
     }
 }
