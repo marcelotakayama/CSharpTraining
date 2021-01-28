dd9c876b479de8849b63a085e583871c644b359f
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 09:54:00 -0300

    quadrado e ao cubo

--- QuadradoAoCubo/QuadradoAoCubo/Program.cs
+++ QuadradoAoCubo/QuadradoAoCubo/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace QuadradoAoCubo {
     class Program {
         static void Main(string[] args) {
+            int entrada = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=entrada; i++) {
+                Console.WriteLine(i + " " + (Math.Pow(i, 2)) + " " + (Math.Pow(i, 3)));
+            }
         }
     }
 }
