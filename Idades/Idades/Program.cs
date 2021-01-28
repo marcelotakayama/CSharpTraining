63c92bae6e6df158c209966805a80867d979197d
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 16:24:09 -0300

    hohoho

--- Idades/Idades/Program.cs
+++ Idades/Idades/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 namespace Idades {
     class Program {
         static void Main(string[] args) {
+            int total = 0;
+            int qtd = 0;
+            while (true) {
+                int entrada = int.Parse(Console.ReadLine());
+                if (entrada < 0) {
+                    break;
+                } else {
+                    total += entrada;
+                    qtd++;
+                }
+            }
+            Console.WriteLine(total/qtd);
         }
     }
 }
