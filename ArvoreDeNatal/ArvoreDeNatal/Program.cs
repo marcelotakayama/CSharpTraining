d8cec164716948b131220a29c2abb386462b418b
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 17:37:13 -0300

    dividindo x por y

--- ArvoreDeNatal/ArvoreDeNatal/Program.cs
+++ ArvoreDeNatal/ArvoreDeNatal/Program.cs
@@ -7,6 +7,37 @@ using System.Threading.Tasks;
 namespace ArvoreDeNatal {
     class Program {
         static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            int t = n / 2;
+
+            while(i <= n) {
+                for(int j=0; j<t; j++) {
+                    Console.WriteLine(" ");
+                }
+                for(int j=0; j<i; j++) {
+                    Console.WriteLine("*");
+                }
+
+                i += 2;
+                t--;
+                Console.WriteLine("\n");
+            }
+            i = 1;
+            t = n / 2;
+
+            while(i <= 3) {
+                for(int j=0; j<t; j++) {
+                    Console.WriteLine(" ");
+                }
+                for(int j=0; j<i; j++) {
+                    Console.WriteLine("*");
+                }
+                i += 2;
+                t--;
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n");
         }
     }
 }
