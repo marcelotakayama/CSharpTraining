db8cc89982089c32ae83f3f33123c95989fdb3df
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 17:47:03 -0300

    dividindo x por y

--- DividindoXporY/DividindoXporY/Program.cs
+++ DividindoXporY/DividindoXporY/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 namespace DividindoXporY {
     class Program {
         static void Main(string[] args) {
+            int qtd = int.Parse(Console.ReadLine());
+            for(int i=0; i<qtd; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+                int n1 = int.Parse(vet[0]);
+                int n2 = int.Parse(vet[1]);
+
+                if(n1 == 0 || n2 == 0) {
+                    Console.WriteLine("Divisão impossivel");
+                } else {
+                    Console.WriteLine(n1/n2);
+                }
+            }
         }
     }
 }
