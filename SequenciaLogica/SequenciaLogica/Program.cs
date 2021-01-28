5c3eb7189221b6951370fb78ce97134b7c26442f
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 10:12:57 -0300

    sequencia logica

--- SequenciaLogica/SequenciaLogica/Program.cs
+++ SequenciaLogica/SequenciaLogica/Program.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 namespace SequenciaLogica {
     class Program {
         static void Main(string[] args) {
+            int cont = 0;
             int entrada = int.Parse(Console.ReadLine());
+            for(int i=0; i<= (entrada*2); i++) {
+                Console.WriteLine(i + 2);
+            }
         }
     }
 }
